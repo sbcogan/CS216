2b20bcb3d25e74dc22e6c25bd0bcf0593c8f983f
sbcogan <sarahcogan1@gmail.com>
2017-03-24 16:52:55 -0400

    stripped of non-alpha chars

--- parseJames.py
+++ parseJames.py
@@ -1,8 +1,6 @@
 import re
-
 allWords = list()
 verses = list()
-
 def clean():
     #ensures each verse begins on its one line, separates each verse with newline if not already done in text
     with open("kingjames.txt", 'r') as bible, open('cleanedBible.txt', 'w') as cleaned:
@@ -15,13 +13,11 @@ def clean():
                         break
                 if not lineBreaks:
                     cleaned.write(line)
-
 def parseJames():
     with open("cleanedBible.txt", 'r') as bible, open('parsed.txt', 'w') as parsed:
         for line in bible:
             getWords(line)
             parsed.write(line)
-
 def getWords(line):
     words = line.split()
     if len(words) > 0:
@@ -31,17 +27,11 @@ def getWords(line):
     for i in range(len(words)):
         if words[i].isalpha():
             allWords.append(words[i])
-
             #need to check for chars like dashes, apostraphes, etc. (wouldn't otherwise evaluate to true with isalpha)
-
 def uniqueWords():
     print(len(set(allWords)))
-
 def numWords():
     print(len(allWords))
-
 clean()
 parseJames()
-
-
 
