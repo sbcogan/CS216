b9a911dd0064452527ec7662e190a6896c7a02ec
sbcogan <sarahcogan1@gmail.com>
2017-03-24 16:54:48 -0400

    intended commit - last one messed up idk

--- parseJames.py
+++ parseJames.py
@@ -1,6 +1,8 @@
 import re
+
 allWords = list()
 verses = list()
+
 def clean():
     #ensures each verse begins on its one line, separates each verse with newline if not already done in text
     with open("kingjames.txt", 'r') as bible, open('cleanedBible.txt', 'w') as cleaned:
@@ -13,25 +15,33 @@ def clean():
                         break
                 if not lineBreaks:
                     cleaned.write(line)
+
 def parseJames():
     with open("cleanedBible.txt", 'r') as bible, open('parsed.txt', 'w') as parsed:
         for line in bible:
             getWords(line)
             parsed.write(line)
+
 def getWords(line):
     words = line.split()
     if len(words) > 0:
         if re.match("^[0-9:]*$",words[0]):
             verse = words[0]
             verses.append(verse)
-    for i in range(len(words)):
-        if words[i].isalpha():
-            allWords.append(words[i])
-            #need to check for chars like dashes, apostraphes, etc. (wouldn't otherwise evaluate to true with isalpha)
+        for i in range(1, len(words)):
+            allWords.append(words[i].strip(",:;.?!"))
+
+            #strips of some chars -- how to deal w apostrophes still not decided
+
 def uniqueWords():
     print(len(set(allWords)))
+
 def numWords():
     print(len(allWords))
+
 clean()
 parseJames()
+print(sorted(set(allWords)))
+
+
 
