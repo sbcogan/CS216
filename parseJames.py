9785f87249a69de8588810d47e7a239d1c9ee1af
sbcogan <sarahcogan1@gmail.com>
2017-03-27 02:08:42 -0400

    still wrong but closer

--- parseJames.py
+++ parseJames.py
@@ -8,19 +8,25 @@ def clean():
     with open("kingjames.txt", 'r') as bible, open('cleanedBible.txt', 'w') as cleaned:
         for line in bible:
                 lineBreaks = False
-                for i in range(5, len(line)):
-                    if line[i].isnumeric():
-                        cleaned.write(line[:i] + "\n" + "\n" + line[i:])
+                if re.match("^[0-9]*$",line[0]):
+                    cleaned.write("\n")
+                line = line.strip("\n")
+                for i in range(6, len(line)):
+                    if line[i].isnumeric() or re.match("^[0-9]*$",line[i]) or line[i] == "6": #sorry this is hardcoded, randomly one 6 wasnt making this conditional true idek
+                        print(line[i] + " " + line)
+                        cleaned.write(line[:i] + "\n" + "\n" + line[i:] + " ")
                         lineBreaks = True
                         break
-                if not lineBreaks:
-                    cleaned.write(line)
+                if not lineBreaks and len(line) > 1:
+                    cleaned.write(line + " ")
+
 
 def parseJames():
     with open("cleanedBible.txt", 'r') as bible, open('parsed.txt', 'w') as parsed:
         for line in bible:
             getWords(line)
-            parsed.write(line + "\n")
+            parsed.write(line)
+
 
 def getWords(line):
     words = line.split()
@@ -44,21 +50,21 @@ def toCSV():
         csv.write("Book, Chapter, Verse Number, Verse Text\n") #header for csv
         book = ""
         for line in bible:
-            if len(line) > 0 and not line[0].isnumeric():
-                if line[0] != "" and line[0] != "\n" and line[0] != " ":
-                    book = line
-                    print(book)
+            if line[:4] == "The ":
+                book = line
                 continue
-            line = line.replace(",", "")
-            words = line.split()
-            nums = words[0].split(":")
-            csv.write(book + "," + nums[0] + "," + nums[1] + "," + line[len(words[0]) + 1:])
+            if line.find(":") > -1:
+                line = line.replace(",", "")
+                words = line.split()
+                nums = words[0].split(":")
+                #print(line)
+                csv.write(book + "," + nums[0] + "," + nums[1] + "," + line[len(words[0]) + 1:])
+                
 
 clean()
 parseJames()
 toCSV()
 #print(sorted(set(allWords)))
 
-#need to ignore line breaks in the original and only add them in at end of verse
+#fixed the newline problem!
 #fixing this I think will help with the bible csv column issue too
-
